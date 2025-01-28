96bdd503c06dddd9711270c77e2ed5e5a8469fbf
nikiciadev <nikiciayt@gmail.com>
2025-01-28 08:39:41 +0100

    added example

--- Memory.h
+++ Memory.h
@@ -3,7 +3,6 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
-#include <cstdint>
 
 typedef NTSTATUS(WINAPI* pNtReadVirtualMemory)(HANDLE ProcessHandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToRead, PULONG NumberOfBytesRead);
 typedef NTSTATUS(WINAPI* pNtWriteVirtualMemory)(HANDLE Processhandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToWrite, PULONG NumberOfBytesWritten);
@@ -12,53 +11,103 @@ class Memory {
 private:
 	pNtReadVirtualMemory MRead;
 	pNtWriteVirtualMemory MWrite;
-
-public:
 	std::string processName;
 	DWORD pid;
 	HANDLE handle;
+	DWORD baseAdress;
 
+public:
 	Memory(std::string processName) : processName(processName) {
 		MRead = (pNtReadVirtualMemory) GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtReadVirtualMemory");
 		MWrite = (pNtWriteVirtualMemory) GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtWriteVirtualMemory");
+		setPH();
+		setBase();
+	}
 
-		HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	template<typename T> T read(uintptr_t address) {
+		T buffer{};
+		MRead(handle, (void*) address, &buffer, sizeof(T), NULL);
+		return buffer;
+	}
+
+	template<typename T> void read(uintptr_t address, T* value) {
+		MRead(handle, (void*) address, (void*) value, sizeof(T), NULL);
+	}
+
+	void read(uintptr_t address, void* buffer, size_t size) {
+		MRead(handle, (void*) address, buffer, size, NULL);
+	}
+
+	template <typename T> void write(uintptr_t address, T value) {
+		MWrite(handle, (void*) address, &value, sizeof(T), NULL);
+	}
+
+	void write(uintptr_t address, void* value, size_t size) {
+		MWrite(handle, (void*) address, value, size, NULL);
+	}
+
+	std::string getProcessName() const {
+		return processName;
+	}
+
+	DWORD getPID() const {
+		return pid;
+	}
+
+	HANDLE getHandle() const {
+		return handle;
+	}
+
+	DWORD getBase() const {
+		return baseAdress;
+	}
+
+	~Memory() {
+		if(handle) CloseHandle(handle);
+	}
+
+private: 
+	void setPH() {
+		HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
 
 		if(INVALID_HANDLE_VALUE == hSnapshot) {
 			std::cerr << "Failed to create snapshot! Error code: " << GetLastError() << std::endl;
-		}
-		
-		PROCESSENTRY32 pe32;
-		pe32.dwSize = sizeof(PROCESSENTRY32);
-		if(Process32First(hSnapshot, &pe32)) {
-			do {
-				if(std::wstring(pe32.szExeFile) == std::wstring(processName.begin(), processName.end())) {
-					pid = pe32.th32ProcessID;
-					handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-				}
-			} while(Process32Next(hSnapshot, &pe32));
 		} else {
-			std::cerr << "Failed to enumerate processes! Error code: " << GetLastError() << std::endl;
+			PROCESSENTRY32 pe32;
+			pe32.dwSize = sizeof(PROCESSENTRY32);
+			if(Process32First(hSnapshot, &pe32)) {
+				do {
+					if(std::wstring(pe32.szExeFile) == std::wstring(processName.begin(), processName.end())) {
+						pid = pe32.th32ProcessID;
+						handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+						break;
+					}
+				} while(Process32Next(hSnapshot, &pe32));
+			} else {
+				std::cerr << "Failed to enumerate processes! Error code: " << GetLastError() << std::endl;
+			}
 		}
 
 		if(hSnapshot) CloseHandle(hSnapshot);
 	}
 
-	template<typename T> T Read(uintptr_t address) {
-		T buffer{};
-		MRead(handle, (void*)address, &T, sizeof(T), NULL);
-		return T;
-	}
+	void setBase() {
+		HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+		if(INVALID_HANDLE_VALUE == hSnapshot) {
+			std::cerr << "Failed to create snapshot! Error code: " << GetLastError() << std::endl;
+		} else {
+			MODULEENTRY32 me;
+			me.dwSize = sizeof(me);
 
-	void Read(uintptr_t address, void* buffer, size_t size) {
-		MRead(handle, (void*)address, buffer, size, NULL);
-	}
-
-	template <typename T> T Write(uintptr_t address, T value) {
-		MWrite(handle, (void*)address, &value, sizeof(T), NULL);
-	}
-
-	~Memory() {
-		if(handle) CloseHandle(handle);
+			if(Module32First(hSnapshot, &me)) {
+				do {
+					if(std::wstring(me.szModule) == std::wstring(processName.begin(), processName.end())) {
+						baseAdress = (DWORD) me.modBaseAddr;
+						break;
+					}
+				} while(Module32Next(hSnapshot, &me));
+			}
+		}
+		if(hSnapshot) CloseHandle(hSnapshot);
 	}
 };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #pragma once
 #include "Memory.h"
 
-int main() {
-	Memory mem("Notepad.exe");
-	std::cout << mem.pid << "\n";
-	return 1;
+const int LOCAL_PLAYER_PTR = 0x0017E0A8;
+const int HEALTH = 0xEC;
+const int ARMOR = 0xF0;
+
+DWORD localPlayer;
+
+int main() { // example for assault cube 1.3.0.2
+	Memory mem("ac_client.exe");
+
+	mem.read<DWORD>(mem.getBase() + LOCAL_PLAYER_PTR, &localPlayer);
+
+	while(true) {
+		mem.write<DWORD>(localPlayer + HEALTH, 9999);
+		std::cout << "Armor: " << mem.read<int>(localPlayer + ARMOR) << "\n";
+	}
+
+	return 0;
 }
